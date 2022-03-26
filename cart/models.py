eef02c9059e7a387f99b999497a6a18b1274c76c
iamashwani <sashwani4242@gmail.com>
2022-03-26 18:03:39 +0530

    sds

--- cart/models.py
+++ cart/models.py
@@ -3,11 +3,14 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 from django.utils.crypto import get_random_string
+import os
+import random
+
 class Profile(models.Model):
     
     mobile = models.CharField(max_length=20)
     otp = models.CharField(max_length=6)
     name = models.CharField(max_length=200)
     username = models.CharField(max_length=200)
-    logo = models.ImageField(upload_to ='profile/', blank=True,null = True)
+    logo = models.FileField(upload_to ='profile/')
     profile_id = models.CharField(max_length=200)
--- cart/serializers.py
+++ cart/serializers.py
@@ -32,6 +32,7 @@ import os
 from django.conf import settings
 from django.conf.urls.static import static
 from pathlib import Path
+from django.core.files.storage import FileSystemStorage
 BASE_DIR = Path(__file__).resolve().parent.parent
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -47,6 +48,8 @@ class ProfileSerializer(serializers.ModelSerializer):
             instance = self.Meta.model.objects.update_or_create(**validated_data, defaults=dict(otp=str(random.randint(1000 , 9999))))[0]            
             instance.save()
             return instance
+
+from django.core.files.base import ContentFile
 class VerifyOTPSerializer(serializers.ModelSerializer):
     
     class Meta:
@@ -54,13 +57,21 @@ class VerifyOTPSerializer(serializers.ModelSerializer):
         fields = ['mobile','otp']
         
     def create(self,validated_data):
+        
         instance = self.Meta.model(**validated_data)
         mywords = "123456789"
         res = "expert@" + str(''.join(random.choices(mywords,k = 6)))
+        # path = os.path.join(BASE_DIR, 'static')
+        # random_logo = random.choice([ x for x in os.listdir(path) if os.path.isfile(os.path.join(path, x))])
+        #BASE_DIR = Path(__file__).resolve().parent.parent
         path = os.path.join(BASE_DIR, 'static')
-        random_logo = random.choice([ x for x in os.listdir(path) if os.path.isfile(os.path.join(path, x))])
-        instance = self.Meta.model.objects.update_or_create(**validated_data, defaults = dict(username = res,name = instance.mobile ,logo = random_logo,profile_id = res))[0]
-        
+        dir_list = os.listdir(path)
+        random_logo = random.choice(dir_list)
+        # file ='logo/'
+        # fs = FileSystemStorage(location = file)
+        # filename = fs.save(random_logo)
+        # file_url = fs.url(filename,)
+        instance = self.Meta.model.objects.update_or_create(**validated_data, defaults = dict(username = res,name = instance.mobile ,logo = random_logo, profile_id = res))[0]
         instance.save()
         return instance
          
